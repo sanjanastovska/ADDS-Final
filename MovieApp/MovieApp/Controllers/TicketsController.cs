9c724479a872e8d9665dab7e4ee80fe7bafd07f6
unknown <nastovska.sanja@gmail.com>
2020-02-08 07:54:41 +0100

    Final

---
+++ MovieApp/MovieApp/Controllers/TicketsController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieApp.Controllers
+{
+    public class TicketsController : Controller
+    {
+        // GET: Tickets
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet] 
+        //public List <string> Get(int id)
+        //{
+        //    return db.
+        //}
+
+        // GET: Tickets/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Tickets/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tickets/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Tickets/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Tickets/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Tickets/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Tickets/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
